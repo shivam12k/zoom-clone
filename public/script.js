cbf18458da060edf84797fd50445478fb7105f37
shivam <63141401+shivam12k@users.noreply.github.com>
2020-11-28 19:37:18 +0530

    fix the bug that calland stream is null in peerjs

--- public/script.js
+++ public/script.js
@@ -1,76 +1,57 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myVideo = document.createElement('video');
+const myPeer = new Peer(undefined, {
+  path: '/peerjs',
+  host: '/',
+  port: '3030'
+})
 let myVideoStream;
+const myVideo = document.createElement('video')
 myVideo.muted = true;
-
-console.log(videoGrid);
-
-const peer = new Peer(undefined, {
-    path: '/peerjs',
-    host: '/',
-    port: '3030'
-});
-
-
-
-// adding  media 
-
+const peers = {}
 navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
+  video: true,
+  audio: true
 }).then(stream => {
-    myVideoStream = stream;
-    addVideoStream(myVideo, stream);
-
-    peer.on('call', call => {
-        call.answer(stream)
-        // const video = document.createElement('video')
-        // call.on('stream', userVideoStream => {
-        //     addVideoStream(video, userVideoStream);
-        // })
-    })
-
-    socket.on('user-connected', (userId, stream) => {
-        connectToNewUser(userId, stream);
-    })
-
-})
-
-
-//socket 
-
-
-peer.on('open', id => {
-
-    socket.emit('join-room', ROOM_ID, id);
-   
-})
-
-const connectToNewUser = (userId, stream) => {
-    // console.log("new user is connected of user id", userId);
-     console.log("stream", stream)
-    const call = peer.call(userId, stream)
+  myVideoStream = stream;
+  addVideoStream(myVideo, stream)
+  myPeer.on('call', call => {
+    call.answer(stream)
     const video = document.createElement('video')
-  
-     call.on('stream', userVideoStream => {
-         console.log("calling")
-        addVideoStream(video, userVideoStream);
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
     })
+  })
 
-    call.on('close', () => {
-        console.log("video remove")
-        video.remove()
-    })
+  socket.on('user-connected', userId => {
+    connectToNewUser(userId, stream)
+  })
+})
+
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+
+  const call = myPeer.call(userId, stream)
+  const video = document.createElement('video')
+console.log(call);
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
+  call.on('close', () => {
+    video.remove()
+  })
+
+  peers[userId] = call
 }
 
-// function which render  video
-const addVideoStream = (video, stream) => {
-    console.log("video stream")
-    video.srcObject = stream;
-    video.addEventListener('loadedmetadata', () => {
-        video.play();
-
-    })
-    videoGrid.append(video);
+function addVideoStream(video, stream) {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video)
 }
